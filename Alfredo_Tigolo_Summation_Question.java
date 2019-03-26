5e7b6e9cbc73e71867a968602a344daa3541b371
AlfredoTigolo <alfredo.tigolo@yahoo.com>
2019-03-26 06:41:41 -0700

    added changes to repository
    still need to work out the logic of the basis

--- Alfredo_Tigolo_Summation_Question.java
+++ Alfredo_Tigolo_Summation_Question.java
@@ -44,24 +44,68 @@ java Alfredo_Tigolo_Summation
 */
 class Alfredo_Tigolo_Summation
 {
-	public static void printArray ( int [] inArray )
-	{
-		for ( int i = 0; i < inArray.length; i++ )
-		{
-			System.out.println ( inArray [ i ]);
+
+	/**
+	 * used to simply print elements
+	 */
+	private static final String HELLO_THOMSON_REUTERS = "Hello Thomson Reuters \n Alfredo.Tigolo@yahoo.com \n 424-646-3493 \n ";
+
+	/*
+	 * Function that takes an array and prints the contents of the array
+	 */
+	public static void printArray(int[] inArray) {
+
+		System.out.print("Input [ ");
+		for (int i = 0; i < inArray.length; i++) {
+			if (inArray.length - 1 == i)
+				System.out.print(inArray[i] + " ");
+			else
+				System.out.print(inArray[i] + ", ");
 		}
-		
+		System.out.print("]");
+
 	}
-	public static void main (String[] args)
-	{
-		System.out.println("Hello Thomson Reuters");
+
+	/*
+	* Sum program that looks for max, len, and array as inputs
+	*/
+	public static void sumArray(int[] inArray, int len, int max) {
+		System.out.println();
+		int best = 0, current = 0;
+		int i = 0;
+		// while ( j >= 0)
+		// {
+		for (i = 0; i < len; ++i) {
+			// current += *(vector + i);
+			current += inArray[i]; // returns the max
+			System.out.println("Current = " + current);
+			// if(current < 0)
+			if (current < max) {
+				current = 0;
+			}
+			// best = best > current ? best : current;
+			best = best > max ? best : max;
+		}
+		System.out.println("max = " + best);
+		// }
+	}
+
+	public static void main(String[] args) {
+		System.out.println(HELLO_THOMSON_REUTERS);
 		//- Input
 		//- array = [5, 10, 3, -4]
-		int[] anArray = {5, 10, 3, -4};
+		int[] anArray1 = {5, 10, 3, -4};
+		int[] anArray2 = {1, 1, 2, 2, 3, 4, 5};
 		//- summation = 2
 		//- max = 8
 
-		printArray ( anArray );
+		printArray ( anArray1 );
+		sumArray ( anArray1, 2, 8 );
+
+		System.out.println();
+
+		printArray ( anArray2 );
+		sumArray ( anArray2, 3, 5 );
 //<<<<<<< HEAD
 
 		// Algorithm coded in C/C++ added
